e2e676b535f33139d7f0b9cc508a268becc21c30
Soorria Saruva <soorria.ss@gmail.com>
2021-11-17 15:35:13 +1100

    Add public to cache-control

--- src/pages/api/script.ts
+++ src/pages/api/script.ts
@@ -5,7 +5,7 @@ const handler: NextApiHandler = async (req, res) => {
   const { s } = req.query
 
   const script = await buildWidgetScript(typeof s === 'string' ? s : '')
-  res.setHeader('Cache-Control', 's-max-age=2678400')
+  res.setHeader('Cache-Control', 'public, s-max-age=2678400')
   res.setHeader('Content-Type', 'application/javascript')
 
   res.end(script)
