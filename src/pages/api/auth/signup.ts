82fd2413cd532fadb4569ec88dbceaa036a4624c
Soorria Saruva <soorria.ss@gmail.com>
2022-01-08 19:30:51 +1100

    Fix typo

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -3,7 +3,7 @@ import { createApiHandler } from '@/lib/api.server'
 import { signupBodySchema, signupUser } from '@/lib/auth.server'
 
 export default createApiHandler(async (req, res) => {
-  if (config.enableSignups && req.method === 'POST') {
+  if (config.enableSignup && req.method === 'POST') {
     const { name, email, password } = await signupBodySchema.parseAsync(req.body)
 
     await signupUser(name, email, password)
