629555403ce2a36ec1fcd096d5eeede5a4fe88e9
Soorria Saruva <soorria.ss@gmail.com>
2024-05-08 00:43:21 +1000

    fix: try write file directly

--- src/lib/widget.server.ts
+++ src/lib/widget.server.ts
@@ -1,12 +1,13 @@
-// import { promises as fs } from 'fs'
-// import path from 'path'
+import { promises as fs } from 'fs'
+import path from 'path'
 import { env } from '~/env'
 
 // const WIDGET_PATH = path.join(process.cwd(), 'widget', 'dist', 'script.js')
-// const WIDGET_PATH = path.join(process.cwd(), 'public', 'script.js')
+const WIDGET_PATH = path.join(process.cwd(), 'public', 'script.js')
 
 export const buildWidgetScript = async (): Promise<string> => {
-  const script = await fetch(`${env.NEXT_PUBLIC_BASE_URL}/script.js`).then(res => res.text())
+  const script = await fs.readFile(WIDGET_PATH, 'utf-8')
+  // const script = await fetch(`${env.NEXT_PUBLIC_BASE_URL}/script.js`).then(res => res.text())
 
   return [`window._AQRM_BASE_URL="${env.NEXT_PUBLIC_BASE_URL}"`, script].join(';')
 }
